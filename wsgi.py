182251019313d7748db338c903590ff57e94de5a
Manos Syngarefs <manos_bass@hotmail.com>
2016-07-24 15:44:57 +0200

    openshift

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
-import os
+import os, sys
+
+sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR']))
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lifeopenshift.settings'
 
 virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+
+os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python2.7/site-packages')
+
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
