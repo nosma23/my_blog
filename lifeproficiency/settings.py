fe9cc388b66ad23e2511501a158b9316c54dc536
Manos Syngarefs <manos_bass@hotmail.com>
2016-09-16 16:10:45 +0300

    database edited

--- lifeproficiency/settings.py
+++ lifeproficiency/settings.py
@@ -25,6 +25,9 @@ SECRET_KEY = '@7qb31kb2l(e*gkn6qi_6#x6fba($6!#a9=pvq5x$oz4%=_bq8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 SITE_ID = 1
 # Application definition
 
@@ -146,10 +149,10 @@ EMAIL_PORT = 587
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+
