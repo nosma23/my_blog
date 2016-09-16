112bdde68f90beb8814f661ef388eeb8b18ac211
Manos Syngarefs <manos_bass@hotmail.com>
2016-09-16 18:53:35 +0300

    settings folder created

--- lifeproficiency/settings/production.py
+++ lifeproficiency/settings/production.py
@@ -19,6 +19,12 @@ if not settings.DEBUG:
         'blog',
     ]
 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(settings.BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
     try:
         import dj_database_url
