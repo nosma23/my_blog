051f56ee86b75cf39fbe37c52d0d9d511cdaf862
Manos Syngarefs <manos_bass@hotmail.com>
2016-09-19 18:09:34 +0300

    wsgi edited

--- lifeproficiency/wsgi.py
+++ lifeproficiency/wsgi.py
@@ -15,9 +15,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lifeproficiency.settings")
 
 application = get_wsgi_application()
 
-if not settings.DEBUG:
-    try:
-        from whitenoise.django import DjangoWhiteNoise
-        application = DjangoWhiteNoise(get_wsgi_application())
-    except:
-        pass
+# if not settings.DEBUG:
+#     try:
+#         from whitenoise.django import DjangoWhiteNoise
+#         application = DjangoWhiteNoise(get_wsgi_application())
+#     except:
+#         pass
