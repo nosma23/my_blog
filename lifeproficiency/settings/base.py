5abb5cec54b85f89a5bc1e732d9bd899714d0995
Manos Syngarefs <manos_bass@hotmail.com>
2016-09-22 16:49:22 +0300

    static and media files moved down one directory

--- lifeproficiency/settings/base.py
+++ lifeproficiency/settings/base.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'blog/staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'blog/mediafiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "blog/static"),
+    os.path.join(BASE_DIR, "static"),
     #'/var/www/static/',
 ]
 
