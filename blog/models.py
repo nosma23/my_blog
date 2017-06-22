e9a6b76dfc18b87649679af56815db54f98db665
Argonavt <zaloznyi.sasha@gmail.com>
2017-06-21 21:09:16 -0400

    Creating separate field for thumb images

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from os import path
 from ckeditor.fields import RichTextField
 from django.conf import settings
 from django.db import models
@@ -6,6 +7,8 @@ from django.db.models import permalink
 from django.utils.safestring import mark_safe
 from markdown_deux import markdown
 
+def image_path(instance, filename):
+    return './min/{0}'.format(filename)
 
 class Blog(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
@@ -14,6 +17,8 @@ class Blog(models.Model):
     image = models.ImageField(null=True, blank=True,
                               width_field='width_field',
                               height_field='height_field')
+    thumb_image = models.ImageField(upload_to=image_path, null=True, blank=True)
+
     width_field = models.IntegerField(default=0)
     height_field = models.IntegerField(default=0)
     body = RichTextField()
