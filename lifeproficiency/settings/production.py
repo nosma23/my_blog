ea6a7554e3245a6a8b5fc21d90721c57a487d376
Manos Syngarefs <manos_bass@hotmail.com>
2017-07-24 19:08:34 +0200

    settings changed

--- lifeproficiency/settings/production.py
+++ lifeproficiency/settings/production.py
@@ -68,5 +68,5 @@ if not settings.DEBUG:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', True)
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', True)
+    EMAIL_HOST_USER = os.environ.['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ.['EMAIL_HOST_PASSWORD']
